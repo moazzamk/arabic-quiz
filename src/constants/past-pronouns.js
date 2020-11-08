3e55e5fb228c1052e765b49062e8e9813b37801e
moazzam khan <moazzam.khan@dialexa.com>
2020-11-08 15:04:05 -0600

    initial commit

---
+++ src/constants/past-pronouns.js
@@ -0,0 +1,74 @@
+const pastProunouns = [
+  // 3m
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u064E',
+    pronoun: 'He',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u064E\u0627',
+    pronoun: 'Both of them',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0648\u0627',
+    pronoun: 'All of them',
+    tense: 'past'
+  },
+  // 3f
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u064E\u062A\u0652',
+    pronoun: 'She',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u064E\u062A\u0652',
+    pronoun: 'Both of them ladies',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u0646\u0618',
+    pronoun: 'All of them ladies',
+    tense: 'past'
+  },
+  // 2m
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u062A\u0618',
+    pronoun: 'You',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u062A\u064F\u0645\u0627',
+    pronoun: 'Both of you',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u062A\u064F\u0645\u0652',
+    pronoun: 'All of you',
+    tense: 'past'
+  },
+  // 2f
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u062A\u061A',
+    pronoun: 'You (fem.)',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u062A\u064F\u0646\u0651\u064E',
+    pronoun: 'All of you ladies',
+    tense: 'past'
+  },
+  // 1p
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u062A\u064F',
+    pronoun: 'I',
+    tense: 'past'
+  },
+  {
+    pattern: '{0}\u064E{1}\u064E{2}\u0652\u0646\u0627',
+    pronoun: 'We',
+    tense: 'past'
+  }
+];
+
+export default pastProunouns;
